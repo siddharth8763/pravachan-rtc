ddb820cbfe90a4950a87f3f4c45dfdaee90a9a2a
Siddharth <siddharth8763@gmail.com>
2023-03-29 22:06:51 +0530

    Created Card Component, Button Component, Refactored the Home Component Code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import styles from "./Home.module.css";
+import { Link, useNavigate  } from "react-router-dom";
+import Card from "../../Components/Shared/Card/Card";
+import Button from "../../Components/Shared/Button/Button";
 
 const Home = () => {
+  const signInLinkStyle = {
+    color: "#0077ff",
+    fontWeight: "bold",
+    textDecoration: "none",
+    marginLeft: "10px",
+  };
+  const navigate = useNavigate();
+  function startRegister() {
+      //navigate('/register');
+  }
   return (
-    <div>Home</div>
-  )
-}
+    <div className={styles.cardWrapper}>
+      <Card title="Welcome to PRAVACHAN" icon="logo">
+        <p className={styles.text}>
+          We’re working hard to get PRAVACHAN ready for everyone! While we
+          wrap up the finishing touches, we’re adding people gradually to make
+          sure nothing breaks
+        </p>
+        <div>
+          <Button onClick={startRegister} text="Get your username" />
+        </div>
+        <div className={styles.signinWrapper}>
+          <span className={styles.hasInvite}>Have an invite text?</span>
+          <Link style={signInLinkStyle} to="/login">
+            Sign in
+          </Link>
+        </div>
+      </Card>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
